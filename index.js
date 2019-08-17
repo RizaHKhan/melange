ca80bc23fb6db0dc762648f3cb67b18550e6fb74
riza <khanriza@gmail.com>
2019-08-17 09:13:56 -0400

    adjustments for heroku

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+let port = process.env.PORT
+if (port == null || port == '') {
+    port = 3000
+}
+
 app.use(express.static('public'))
 app.set('views', 'templates')
 app.set('view engine', 'ejs')
@@ -9,4 +14,4 @@ app.get('/', function(req, res) {
     res.render('landing')
 })
 
-app.listen(3000);
+app.listen(port);
